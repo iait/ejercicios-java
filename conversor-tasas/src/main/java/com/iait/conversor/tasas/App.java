db07e9195e64398c4a623af544fd26ae416b9802
Ismael Ait <iait@eiva.com.ar>
2019-09-18 16:54:52 -0300

    Inicializa el proyecto

---
+++ conversor-tasas/src/main/java/com/iait/conversor/tasas/App.java
@@ -0,0 +1,23 @@
+package com.iait.conversor.tasas;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        LOG.debug("Inicializa la aplicación de conversor de tasas");
+    }
+}
